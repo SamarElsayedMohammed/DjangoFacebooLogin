cfeefb91fcc14b46102de7614e63287a9a9452a1
SamarElsayedMohammed <samarelsayedmohammed12345@gmail.com>
2022-02-18 03:38:46 +0200

    first

---
+++ FacebookLogin/views.py
@@ -0,0 +1,8 @@
+from django.http import request
+from django.shortcuts import render
+
+def home(request):
+    return render(request,'home.html')
+
+def profile(request):
+    return render(request,'profile.html')
